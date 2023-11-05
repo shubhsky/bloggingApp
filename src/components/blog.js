bdbd1fbaee336a49079674adb1636715a3d5618d
Shubham Kumar <shubhamkr15060@gmail.com>
2023-11-06 00:37:31 +0530

    setBlogs

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,6 +1,6 @@
 import {useReducer, useState, useEffect, useRef} from "react";
 import {db} from "../fireBaseInit";
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, doc, setDoc} from "firebase/firestore"; 
 
 function blogsReducer(blogs, action){
     switch(action.type){
@@ -38,12 +38,14 @@ export default function Blog(){
         dispatch({type:'ADD', blog:{title:formData.title, content:formData.content}})
 
         // Add a new document with a generated id.
-        const docRef = await addDoc(collection(db, "blogs"), {
+
+        const docRef = doc(collection(db,'blogs'))
+
+        await setDoc(docRef, {
             title: formData.title,
             content: formData.content,
             createAt: new Date()
         });
-        console.log("Document written with ID: ", docRef.id);
 
         setFormData({title:'',content:''});
         titleRef.current.focus();
